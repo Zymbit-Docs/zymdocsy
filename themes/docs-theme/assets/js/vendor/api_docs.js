8d0c5495a9650eaa90dd11401c498eed6a4abad3
Nick Anderegg <nick.anderegg@zymbit.com>
2021-04-16 18:03:11 -0400

    feat: preliminary styling for API docs
    Signed-off-by: Nick Anderegg <nick.anderegg@zymbit.com>

---
+++ themes/docs-theme/assets/js/vendor/api_docs.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.querySelectorAll("div.class div.method > .signature").forEach(
+        signatureBox => {
+            const boxHeight = signatureBox.clientHeight
+            const textElem = signatureBox.querySelector(".name")
+            const textElemHeight = textElem.offsetHeight
+            console.log([boxHeight, textElemHeight])
+            if (boxHeight >= (textElemHeight * 1.5)) {
+                signatureBox.classList.add("wrapped-method-signature")
+                // signatureBox.querySelectorAll(".param-list .param").forEach(
+                //     paramElem => {
+                //         paramElem.classList.add("wrapped-param")
+                //     }
+                // )
+            }
+        }
+    )
+})
